470b279bf1459f17a74bdb059de65ddb1fc4d790
Nada Jaradat <86928581+nadajaradat@users.noreply.github.com>
2024-07-28 11:13:12 +0300

    BE-3: Handle logout (#3)
    Co-authored-by: nadajrt <nadaj@brocali.co>

--- src/routes/api.php
+++ src/routes/api.php
@@ -15,6 +15,8 @@ Route::post('/login', [AuthController::class, 'login']);
 
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
     Route::controller(UserController::class)->group(function () {
         Route::get('/user', 'index');
         Route::post('/user', 'store');
